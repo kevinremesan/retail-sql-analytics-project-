a68d7eb79d088e6f9fcd08358c8fc34f6e992095
kevinremesan <kevin18994@gmail.com>
2026-01-02 11:36:15 +0000

    Update 03_create_dim_product.sql

--- sql/sql/warehouse/03_create_dim_product.sql
+++ sql/sql/warehouse/03_create_dim_product.sql
@@ -3,3 +3,7 @@ CREATE TABLE dim_product (
     stock_code VARCHAR(50),
     product_name VARCHAR(255)
 );
+
+INSERT INTO dim_product (stock_code, product_name)
+SELECT DISTINCT stock_code, description
+FROM stg_online_retail_clean;
