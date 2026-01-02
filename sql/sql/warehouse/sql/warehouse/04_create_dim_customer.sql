3471a792d8ca01576200d35ec0979d6170de0839
kevinremesan <kevin18994@gmail.com>
2026-01-02 11:36:42 +0000

    Update 04_create_dim_customer.sql

--- sql/sql/warehouse/sql/warehouse/04_create_dim_customer.sql
+++ sql/sql/warehouse/sql/warehouse/04_create_dim_customer.sql
@@ -2,3 +2,8 @@ CREATE TABLE dim_customer (
     customer_id VARCHAR(50) PRIMARY KEY,
     country VARCHAR(100)
 );
+
+INSERT INTO dim_customer (customer_id, country)
+SELECT DISTINCT customer_id, country
+FROM stg_online_retail_clean
+WHERE customer_id IS NOT NULL;
