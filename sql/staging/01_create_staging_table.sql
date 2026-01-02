0b1778be9ca75350db313da4690f45682ee988b0
kevinremesan <kevin18994@gmail.com>
2026-01-02 11:27:51 +0000

    Create 01_create_staging_table.sql

---
+++ sql/staging/01_create_staging_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE stg_online_retail (
+    invoice_no      VARCHAR(50),
+    stock_code      VARCHAR(50),
+    description     VARCHAR(255),
+    quantity        VARCHAR(50),
+    invoice_date    VARCHAR(50),
+    unit_price      VARCHAR(50),
+    customer_id     VARCHAR(50),
+    country         VARCHAR(50)
+);
