e67f0d575c5e89a55929a52723db4dd70699584e
kevinremesan <kevin18994@gmail.com>
2026-01-02 12:08:23 +0000

    Delete sql/sql directory

--- sql/sql/warehouse/03_create_dim_product.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dim_product (
-    product_id INT IDENTITY PRIMARY KEY,
-    stock_code VARCHAR(50),
-    product_name VARCHAR(255)
-);
-
-INSERT INTO dim_product (stock_code, product_name)
-SELECT DISTINCT stock_code, description
-FROM stg_online_retail_clean;
